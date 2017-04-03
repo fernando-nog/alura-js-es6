ab434c4cbfe4392b0ae8476cad772c48fc3bc2ea
Fernando Nogueira <fernando.nogueira@mv.com.br>
2017-04-03 15:08:49 -0300

    Aula 5 da parte 4

--- client/js/app/controllers/NegociacaoController.js
+++ client/js/app/controllers/NegociacaoController.js
@@ -10,23 +10,14 @@ class NegociacaoController {
 
     adiciona(event){
         event.preventDefault();
-
-        let data = new Date(
-            ...this._inputData.value
-                .split('-')
-                .map( (item, indice) =>  item - indice % 2 )
-        );
-        console.log(data);
-
+        
         let negociacao = new Negociacao(
-            data, 
+            DateHelper.textoParaData(this._inputData.value), 
             this._inputQuantidade.value,
             this._inputValor.value  
-        )
-        let diaMesAno = negociacao.data.getDate() 
-            + '/' + (negociacao.data.getMonth() +1);
-            + '/' + negociacao.data.getFullYear();
-    
-        console.log('Data formatada: '+diaMesAno);
+        );
+
+        console.log(negociacao);
+        console.log('Data formatada: '+DateHelper.dataParaTexto(negociacao.data));
     }
 }
