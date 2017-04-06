2be486d31b4bfbbe594e31ce6f114114672f0bb7
Fernando Nogueira <fernando.nogueira@mv.com.br>
2017-04-06 17:11:48 -0300

    limpando

--- client/js/app/views/NegociacoesView.js
+++ client/js/app/views/NegociacoesView.js
@@ -32,9 +32,7 @@ class NegociacaoView {
                 <tfoot>
                     <td colspan="3"></td>
                     <td>
-                        ${
-                           model.negociacoes.reduce((total, n) => total + n.volume, 0.0)
-                        }
+                        ${model.negociacoes.reduce((total, n) => total + n.volume, 0.0)}
                     </td>
                 </tfoot>
             </table>
