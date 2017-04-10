ef437031a6a7ed1c15faf0149d92940e0253092e
Fernando Nogueira <fernando.nogueira@mv.com.br>
2017-04-10 17:48:19 -0300

    Add msgView

--- client/js/app/controllers/NegociacaoController.js
+++ client/js/app/controllers/NegociacaoController.js
@@ -7,17 +7,25 @@ class NegociacaoController {
         this._inputValor = $('#valor');
         this._listaNegociacoes = new ListaNegociacoes();
         this._negociacoesView = new NegociacaoView($('#negociacoesView'));
+        this._mensagem = new Mensagem();
 
         this._negociacoesView.update(this._listaNegociacoes);
+        
+        this._mensagemView = new MensagemView( $('#mensagemView') );
+        this._mensagemView.update(this._mensagem);
     }
 
     adiciona(event){
         event.preventDefault();
         
         this._listaNegociacoes.adiciona(this._criaNegociacao());
+        this._negociacoesView.update(this._listaNegociacoes);
+
+        this._mensagem.texto = 'Negociação adicionada com sucesso.';
+        this._mensagemView.update(this._mensagem);
         this._limpaFormulario();
 
-        this._negociacoesView.update(this._listaNegociacoes);
+       
     }
 
     _criaNegociacao(){
