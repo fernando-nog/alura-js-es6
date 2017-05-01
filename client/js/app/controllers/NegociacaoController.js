345df1203e34e7dfabb83848d000067592c0c45e
Fernando Nogueira <fernando.a.gomes.n@gmail.com>
2017-05-01 20:12:40 -0300

    Add listar/apagar todos com indexDB - Fim aula 3

--- client/js/app/controllers/NegociacaoController.js
+++ client/js/app/controllers/NegociacaoController.js
@@ -17,23 +17,51 @@ class NegociacaoController {
             new MensagemView( $('#mensagemView') ),
             ['texto']
         );
+
+        ConnectionFactory
+            .getConnection()
+            .then(connection => new NegociacaoDAO(connection))
+            .then(dao => dao.listaTodos())
+            .then(negociacoes => 
+                negociacoes.forEach(negociacao =>
+                    this._listaNegociacoes.adiciona(negociacao))
+            ) 
+            .catch((erro) => {
+                console.log(`Erro ao listar todos: ${erro}`);
+                this._mensagem.texto = erro;
+            });
     }
 
     adiciona(event){
-        event.preventDefault();
-        
-        this._listaNegociacoes.adiciona(this._criaNegociacao());
-        this._mensagem.texto = 'Negociação adicionada com sucesso.';
-        this._limpaFormulario();
 
-       
+        
+
+        event.preventDefault();
+
+        ConnectionFactory
+            .getConnection()
+            .then((connection) => {
+                let negociacao = this._criaNegociacao();
+
+                new NegociacaoDAO(connection)
+                    .adiciona(negociacao)
+                    .then( () => {
+                        this._listaNegociacoes.adiciona(negociacao);
+                        this._mensagem.texto = 'Negociação adicionada com sucesso.';
+                        this._limpaFormulario();
+                    })
+            })
+            .catch((erro) => {
+                console.log(erro);
+                this._mensagem.texto = erro;
+            });
     }
 
     _criaNegociacao(){
         return new Negociacao(
             DateHelper.textoParaData(this._inputData.value), 
-            this._inputQuantidade.value,
-            this._inputValor.value  
+            parseInt(this._inputQuantidade.value),
+            parseFloat(this._inputValor.value)  
         );
     }
 
@@ -45,8 +73,14 @@ class NegociacaoController {
     }
 
     apaga(){
-        this._listaNegociacoes.esvazia();
-        this._mensagem.texto = "Negociações apagadas.";
+        ConnectionFactory
+            .getConnection()
+            .then(connection => new NegociacaoDAO(connection))
+            .then(dao => dao.apagaTodos())
+            .then(mensagem => {
+                this._mensagem.texto = mensagem;
+                this._listaNegociacoes.esvazia();
+            })
     }
 
     importaNegociacoes(){
--- client/js/app/dao/NegociacaoDao.js
+++ client/js/app/dao/NegociacaoDao.js
@@ -25,4 +25,50 @@ class NegociacaoDAO {
             };
         });
     }
+
+    listaTodos(){
+
+        return new Promise((resolve, reject) => {
+            let cursor = this._connection
+                .transaction(['negociacoes'],'readwrite')
+                .objectStore('negociacoes')
+                .openCursor();
+
+            let negociacoes = [];
+
+            cursor.onsuccess = (event) => {
+                let ponteiroNegociacao = event.target.result;
+
+                if(ponteiroNegociacao){
+                    var dado =  ponteiroNegociacao.value;
+                    negociacoes.push(new Negociacao(dado._data, dado._quantidade, dado._valor));
+                    ponteiroNegociacao.continue();
+                }else{
+                    resolve(negociacoes);
+                }
+            };
+
+            cursor.onerror = (event) => {
+                console.log(event.target.error.name);
+            };
+        });
+    }
+
+    apagaTodos(){
+        return new Promise((resolve, reject) =>{
+            let request = this._connection
+                .transaction(['negociacoes'],'readwrite')
+                .objectStore('negociacoes')
+                .clear();
+
+            request.onsuccess = (event) => {
+                resolve('Todas as negocições foram apagas com sucesso.');
+            };
+
+            request.onerror = (event) => {
+                console.log(event.target.error);
+                reject('Não foi possível apagar todas a negociações');
+            };
+        });
+    }
 }
